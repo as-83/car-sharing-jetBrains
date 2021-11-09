f14d171b056cda7c29d6a0bcc58fdbf9172af059
abdsul <abdsul3434@gmail.com>
2021-11-09 16:55:28 +0300

    after 3 stage completion.

--- Car Sharing/task/src/carsharing/Main.java
+++ Car Sharing/task/src/carsharing/Main.java
@@ -1,10 +1,5 @@
 package carsharing;
 
-import org.w3c.dom.ls.LSOutput;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,36 +8,25 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        String databaseFileName = "carsharing";
         if (args.length != 0 && args[0].equals("-databaseFileName")) {
-            databaseFileName = args[1];
+            db.setDatabaseFileName(args[1]);
         }
-        Connection connection = db.getConnection(databaseFileName);
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(
-                    "CREATE TABLE " +
-                    //"IF NOT EXISTS " +
-                    "COMPANY(id INTEGER PRIMARY KEY AUTO_INCREMENT," +
-                    "name VARCHAR NOT NULL UNIQUE )");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        db.closeConnection();
+        db.createTables();
+
         int mainMenuSelection = 1;
         while( mainMenuSelection != 0) {
             printMainMenu();
             mainMenuSelection = scanner.nextInt();
             scanner.nextLine();
             if (mainMenuSelection == 1) {
-                int optionsSelection = 1;
-                while(optionsSelection != 0) {
-                    printOptions();
-                    optionsSelection = scanner.nextInt();
+                int managerOptionsSelection = 1;
+                while(managerOptionsSelection != 0) {
+                    printManagerOptions();
+                    managerOptionsSelection = scanner.nextInt();
                     scanner.nextLine();
-                    if (optionsSelection == 1) {
+                    if (managerOptionsSelection == 1) {
                         printCompanies();
-                    } else if (optionsSelection == 2) {
+                    } else if (managerOptionsSelection == 2) {
                         createCompany();
                     }
                 }
@@ -54,6 +38,8 @@ public class Main {
 
     }
 
+
+
     private static void createCompany() {
         System.out.println("Enter the company name:");
         String companyTitle = scanner.nextLine();
@@ -64,15 +50,69 @@ public class Main {
 
     private static void printCompanies() {
         List<String> companies = db.getAllCompanies();
-        companies.forEach(c -> System.out.println(c));
+        if (companies.size() != 0) {
+            int[] number = {1};
+            System.out.println("Choose the company:");
+            companies.forEach(c -> System.out.println(number[0]++ + ". " + c));
+            System.out.println("0. Back");
+            int companyNumber = scanner.nextInt();
+            scanner.nextLine();
+            if (companyNumber != 0) {
+                System.out.println("'" + companies.get(companyNumber - 1) + "' company");
+                printCompanyMenu();
+                selectInCompanyMenu(companies.get(companyNumber - 1));
+            }
+        } else {
+            System.out.println("The company list is empty!");
+        }
     }
 
-    private static void printOptions() {
+    private static void createCar(String companyName) {
+        System.out.println("Enter the car name:");
+        String carName = scanner.nextLine();
+        db.addCar(companyName, carName);
+        System.out.println("The car was added!");
+        printCompanyMenu();
+        selectInCompanyMenu(companyName);
+
+    }
+
+    private static void printCarList(String companyName) {
+        List<String> cars = db.getCompanyCars(companyName);
+        if (cars.size() != 0) {
+            int[] number = {1};
+            System.out.println("Car list:");
+            cars.forEach(c -> System.out.println(number[0]++ + ". " + c));
+
+        } else {
+            System.out.println("The car list is empty!");
+        }
+        printCompanyMenu();
+        selectInCompanyMenu(companyName);
+    }
+
+    private static void printCompanyMenu() {
+        System.out.println(
+                "1. Car list\n" +
+                "2. Create a car\n" +
+                "0. Back");
+    }
+
+    private static void selectInCompanyMenu(String companyName) {
+        int companyMenuSelection = scanner.nextInt();
+        scanner.nextLine();
+        if (companyMenuSelection == 1) {
+            printCarList(companyName);
+        } else if (companyMenuSelection == 2) {
+            createCar(companyName);
+        }
+    }
+
+    private static void printManagerOptions() {
         System.out.println(
                 "1. Company list\n" +
                 "2. Create a company\n" +
-                "0. Back"
-        );
+                "0. Back");
     }
 
     private static void printMainMenu() {
