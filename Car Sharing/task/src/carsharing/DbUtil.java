3499b1b73e37232cad1d819cf85625bb3faa94ae
Suleyman hp <abdsul3434@gmail.com>
2021-11-10 22:53:57 +0300

    hp note commit

--- Car Sharing/task/src/carsharing/DbUtil.java
+++ Car Sharing/task/src/carsharing/DbUtil.java
@@ -1,25 +1,30 @@
 package carsharing;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class DbUtil {
     private static Connection connection = null;
     private static String databaseFilePath = "./src/carsharing/db/";//jdbc:h2:./src/carsharing/db/carsharing
+    private static String databaseFileName = "carsharing";//jdbc:h2:./src/carsharing/db/carsharing
+    private static List<String> cars = new ArrayList<>();
 
-    public Connection getConnection(String databaseFileName) {
+    public void setDatabaseFileName(String databaseFileName) {
+        DbUtil.databaseFileName = databaseFileName;
+    }
+
+    public Connection getConnection() {
         if (connection != null) {
             return connection;
         }
         try {
-            databaseFilePath = databaseFilePath + databaseFileName;
-            connection = DriverManager.getConnection("jdbc:h2:" + databaseFilePath);
+            connection = DriverManager.getConnection("jdbc:h2:" + databaseFilePath + databaseFileName);
         } catch (SQLException se) {
             System.out.println("Connection getting error");
             se.printStackTrace();
         }
-        System.out.println("Connection OK");
         return connection;
     }
 
@@ -27,11 +32,116 @@ public class DbUtil {
         if (connection != null) {
             try {
                 connection.close();
-                System.out.println("Connection CLOSED");
             } catch (SQLException se) {
                 se.printStackTrace();
             }
             connection = null;
         }
+    }
+
+    public void addCompany(String companyTitle) {
+        try {
+            getConnection().createStatement().execute("INSERT INTO COMPANY (name) " +
+                    "VALUES ('" +
+                    companyTitle +
+                    "');");
+            closeConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public List<String> getAllCompanies() {
+        ResultSet resultSet = null;
+        List<String> companies = new ArrayList<>();
+        try {
+            resultSet = getConnection().createStatement().executeQuery("select * from COMPANY " +
+                    "ORDER BY ID");
+
+            while(resultSet.next()) {
+                companies.add(resultSet.getString(2));
+                //System.out.println(resultSet.getString(2));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        closeConnection();
+        return companies;
+    }
+
+    public void createTables() {
+        Connection connection = getConnection();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(
+                    "CREATE TABLE " +
+                            "IF NOT EXISTS " +
+                            "COMPANY(id INTEGER PRIMARY KEY AUTO_INCREMENT," +
+                            "name VARCHAR NOT NULL UNIQUE )");
+            statement.executeUpdate(
+                    "CREATE TABLE " +
+                            "IF NOT EXISTS " +
+                            "CAR (id INTEGER PRIMARY KEY AUTO_INCREMENT," +
+                            "name VARCHAR NOT NULL UNIQUE," +
+                            "company_id INT NOT NULL, " +
+                            "foreign key (company_id) references COMPANY(ID))");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        closeConnection();
+    }
+
+    public void addCar(String companyName, String carName) {
+        try {
+            int companyId = getCompanyIdByName(companyName);
+            getConnection().createStatement().execute("INSERT INTO CAR (name, company_id) " +
+                    "VALUES ('" +
+                    carName +
+                    "', " +
+                    companyId +
+                    ");");
+            closeConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private int getCompanyIdByName(String companyName) {
+        int companyId = 0;
+        try {
+            ResultSet resultSet = getConnection().createStatement().executeQuery("select id from COMPANY " +
+                    "where name = '" +
+                    companyName +
+                    "';");
+            if (resultSet.next()) {
+                companyId = resultSet.getInt("id");
+            }
+            closeConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return companyId;
+    }
+
+    public List<String> getCompanyCars(String companyName) {
+        ResultSet resultSet = null;
+        List<String> cars = new ArrayList<>();
+        try {
+            resultSet = getConnection().createStatement().executeQuery("select c.id, c.name from " +
+                    "COMPANY AS CM INNER JOIN CAR AS C " +
+                    "ON CM.ID = C.COMPANY_ID " +
+                    "WHERE CM.NAME = '" +
+                    companyName +
+                    "' " +
+                    "ORDER BY C.ID");
+
+            while(resultSet.next()) {
+                cars.add(resultSet.getString(2));
+                //System.out.println(resultSet.getString(2));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        closeConnection();
+        return cars;
     }
 }
--- Car Sharing/task/src/carsharing/Main.java
+++ Car Sharing/task/src/carsharing/Main.java
@@ -1,25 +1,128 @@
 package carsharing;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     private static DbUtil db = new DbUtil();
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String databaseFileName = "carsharing";
         if (args.length != 0 && args[0].equals("-databaseFileName")) {
-            databaseFileName = args[1];
+            db.setDatabaseFileName(args[1]);
         }
-        Connection connection = db.getConnection(databaseFileName);
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS COMPANY(" +
-                    "id INTEGER PRIMARY KEY," +
-                    "name VARCHAR NOT NULL)");
-        } catch (SQLException e) {
-            e.printStackTrace();
+        db.createTables();
+
+        int mainMenuSelection = 1;
+        while( mainMenuSelection != 0) {
+            printMainMenu();
+            mainMenuSelection = scanner.nextInt();
+            scanner.nextLine();
+            if (mainMenuSelection == 1) {
+                int managerOptionsSelection = 1;
+                while(managerOptionsSelection != 0) {
+                    printManagerOptions();
+                    managerOptionsSelection = scanner.nextInt();
+                    scanner.nextLine();
+                    if (managerOptionsSelection == 1) {
+                        printCompanies();
+                    } else if (managerOptionsSelection == 2) {
+                        createCompany();
+                    }
+                }
+
+
+            }
         }
-        db.closeConnection();
+
+
     }
+
+
+
+    private static void createCompany() {
+        System.out.println("Enter the company name:");
+        String companyTitle = scanner.nextLine();
+        db.addCompany(companyTitle);
+        System.out.println("The company was created!");
+
+    }
+
+    private static void printCompanies() {
+        List<String> companies = db.getAllCompanies();
+        if (companies.size() != 0) {
+            int[] number = {1};
+            System.out.println("Choose the company:");
+            companies.forEach(c -> System.out.println(number[0]++ + ". " + c));
+            System.out.println("0. Back");
+            int companyNumber = scanner.nextInt();
+            scanner.nextLine();
+            if (companyNumber != 0) {
+                System.out.println("'" + companies.get(companyNumber - 1) + "' company");
+                printCompanyMenu();
+                selectInCompanyMenu(companies.get(companyNumber - 1));
+            }
+        } else {
+            System.out.println("The company list is empty!");
+        }
+    }
+
+    private static void createCar(String companyName) {
+        System.out.println("Enter the car name:");
+        String carName = scanner.nextLine();
+        db.addCar(companyName, carName);
+        System.out.println("The car was added!");
+        printCompanyMenu();
+        selectInCompanyMenu(companyName);
+
+    }
+
+    private static void printCarList(String companyName) {
+        List<String> cars = db.getCompanyCars(companyName);
+        if (cars.size() != 0) {
+            int[] number = {1};
+            System.out.println("Car list:");
+            cars.forEach(c -> System.out.println(number[0]++ + ". " + c));
+
+        } else {
+            System.out.println("The car list is empty!");
+        }
+        printCompanyMenu();
+        selectInCompanyMenu(companyName);
+    }
+
+    private static void printCompanyMenu() {
+        System.out.println(
+                "1. Car list\n" +
+                "2. Create a car\n" +
+                "0. Back");
+    }
+
+    private static void selectInCompanyMenu(String companyName) {
+        int companyMenuSelection = scanner.nextInt();
+        scanner.nextLine();
+        if (companyMenuSelection == 1) {
+            printCarList(companyName);
+        } else if (companyMenuSelection == 2) {
+            createCar(companyName);
+        }
+    }
+
+    private static void printManagerOptions() {
+        System.out.println(
+                "1. Company list\n" +
+                "2. Create a company\n" +
+                "0. Back");
+    }
+
+    private static void printMainMenu() {
+        System.out.println(
+                "1. Log in as a manager\n" +
+                "0. Exit"
+        );
+    }
+
+
+
+
 }
